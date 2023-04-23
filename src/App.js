881f211c2cfe24cc42b2f2dd4f55d65deb930bcf
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-23 10:50:31 +0530

    resize the images and and comparing 2 cards logic added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Card from './components/Card';
+import { useEffect } from 'react';
 
 const cardItems = [
   { "src": "/img/Book.png" },
@@ -14,6 +15,8 @@ const cardItems = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards
   const shuffleCards = () => {
@@ -24,13 +27,42 @@ function App() {
     setTurns(0)
   }
 
+  // handle choice
+  const handleChoice = (card)=>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // reset choice and increase turns
+  const resetTurn = ()=>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurn => prevTurn+1)
+  }
+
+  // compare 2 choice
+  useEffect(()=>{
+    if(choiceOne&&choiceTwo){
+      if(choiceOne.src === choiceTwo.src){
+        console.log('equal cards')
+        resetTurn()
+      }else{
+        console.log('not match')
+        resetTurn()
+      }
+    }
+  },[choiceOne,choiceTwo])
+
   return (
     <div className="App">
       <h1>Memory Master</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
         {cards.map((card) => 
-          <Card key={card.id} card={card}/>
+          <Card 
+          key={card.id} 
+          card={card}
+          handleChoice = {handleChoice}
+          />
         )}
       </div>
       <div>{turns}</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
 import './card.css'
 
-const Card = ({card}) => {
+const Card = ({card,handleChoice}) => {
+    
+    const handleClick = () => {
+        handleChoice(card)
+    }
+    
     return ( 
         <div className="card">
             <div>
               <img className='front' src={card.src} alt="card front" />
-              <img src="./img/Card-Cover-Vector.jpg" alt="card back" className="back" />
+              <img onClick={handleClick} src="./img/Card-Cover-Vector.jpg" alt="card back" className="back" />
             </div>
           </div>
      );
